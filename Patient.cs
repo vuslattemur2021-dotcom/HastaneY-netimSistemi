2b12de46ece305bcdc22102110a583489f9f82df
vuslattemur2021-dotcom <vuslattemur2021@gmail.com>
2026-03-06 21:43:35 +0300

    Create Patient model class

---
+++ Patient.cs
@@ -0,0 +1,10 @@
+public class Patient
+{
+    public int PatientId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
