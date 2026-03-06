2392af0bf8a9edfec8c3188b74e34dd8cc63b005
vuslattemur2021-dotcom <vuslattemur2021@gmail.com>
2026-03-06 21:50:14 +0300

    Create Doctor model class

---
+++ Doctor.cs
@@ -0,0 +1,10 @@
+public class Doctor
+{
+    public int DoctorId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Specialization { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string License { get; set; }
+}
